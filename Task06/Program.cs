cd01968bbab761b6e2b0989c915731c9491d5d56
daboyspace <daboyspace@gmail.com>
2022-11-16 21:31:20 +0300

    Add Info

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -6,3 +6,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
