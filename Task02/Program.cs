a213c861e0b886dd1ab86017556e57dcae6fd4a2
daboyspace <daboyspace@gmail.com>
2022-11-16 19:02:51 +0300

    Add Info

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -6,3 +6,16 @@
 // a = 5 b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"{a}>{b} ");
+}
+else
+{
+    Console.WriteLine($"{b}>{a} ");
+}
