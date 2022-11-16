2f962612cceda36e0725767ef1a591cc3f78df02
daboyspace <daboyspace@gmail.com>
2022-11-16 19:16:30 +0300

    Add Info

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -5,3 +5,13 @@
 // Например:
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int count = 1; count <= num; count++)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count + " ");
+    }
+}
