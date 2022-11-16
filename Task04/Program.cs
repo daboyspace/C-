67651a2dd9b0f167be9b22aac2efe58092c42e73
daboyspace <daboyspace@gmail.com>
2022-11-16 22:14:19 +0300

    Add Info

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -6,3 +6,28 @@
 // 2, 3, 7 -> 7
 // 44, 5, 78 -> 78
 // 22, 3, 9 -> 22
+
+
+Console.WriteLine("Введите по очереди три числа: ");
+Console.Write("1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    if (b > a && b > c)
+    {
+        Console.WriteLine($"max = {b}");
+    }
+    else
+    {
+        if (c > b && c > a)
+            Console.WriteLine($"max = {c}");
+    }
+}
