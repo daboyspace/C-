b295500c1b91478902e4fc6603d5ac4878c45fef
daboyspace <daboyspace@gmail.com>
2022-11-17 15:25:14 +0300

    Add Info Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -12,8 +12,8 @@ Console.WriteLine($"Случайное трёхзначное чило = {number
 int RemoveSecondDigit(int numb)
 {
     int firstDigit = numb / 100;
-    int secondDigit = numb % 10;
-    int result = firstDigit * 10 + secondDigit;
+    int thirdDigit = numb % 10;
+    int result = firstDigit * 10 + thirdDigit;
     return result;
 }
 Console.WriteLine("Удаляем вторую цифру числа");
