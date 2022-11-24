61fa6497175f737f59c0a3f7f6a1d5a5b2a54066
daboyspace <daboyspace@gmail.com>
2022-11-24 18:05:14 +0300

    Add Task 30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -4,3 +4,23 @@
 // Например:
 // [1,0,1,1,0,1,0,0]
 
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
