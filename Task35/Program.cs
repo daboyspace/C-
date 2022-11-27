8215ba63e14b55b300f3afde447b281a806eb90d
daboyspace <daboyspace@gmail.com>
2022-11-27 20:39:48 +0300

    Add Task 35

---
+++ Task35/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(123, 0, 1000);
+PrintArray(array);
+
+int countElements = CountElements(array);
+if (countElements > 0) Console.WriteLine($"Количество двузначных элементов массива равно {countElements}");
+else Console.WriteLine("Двузначных чисел в массиве нет");
