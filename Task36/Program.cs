d7e8d83955bd62a0d58ead1e15d773231841120e
daboyspace <daboyspace@gmail.com>
2022-11-29 13:49:34 +0300

    Add Task 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,19 +5,19 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-Random rnd = new Random();                
-int n = rnd.Next(1, 10);                     
+Random rnd = new Random();
+int n = rnd.Next(1, 10);
 int[] arr = new int[n];
 
-arr =  CreateArray(arr, n);                  
-OutputArray(arr, n);                        
+arr = CreateArray(arr, n);
+OutputArray(arr, n);
 
 Console.WriteLine(SearchSum(arr, n));
 
 
-int[] CreateArray(int[] arr, int n) 
-{        
-    for (int i = 0; i < n; i++) 
+int[] CreateArray(int[] arr, int n)
+{
+    for (int i = 0; i < n; i++)
     {
         arr[i] = rnd.Next(-100, 100);
     }
@@ -25,23 +25,22 @@ int[] CreateArray(int[] arr, int n)
     return arr;
 }
 
-void OutputArray(int[] arr, int n) 
-{         
+void OutputArray(int[] arr, int n)
+{
     Console.Write($"[{arr[0]}, ");
 
-    for (int i = 1; i < n - 1; i++) 
+    for (int i = 1; i < n - 1; i++)
     {
         Console.Write($"{arr[i]}, ");
     }
-    Console.WriteLine($"{arr[n-1]}]");
+    Console.WriteLine($"{arr[n - 1]}]");
 }
 
-int SearchSum(int[] arr, int n) 
-{          
+int SearchSum(int[] arr, int n)
+{
     int sum = 0;
 
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
         if (i % 2 != 0) sum += arr[i];
-    
     return sum;
 }
