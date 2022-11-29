82c1d40942aec579b7dd1b4205c4e78fd4a0f7cb
daboyspace <daboyspace@gmail.com>
2022-11-29 13:05:27 +0300

    Add Task 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -11,11 +11,11 @@ double[] arr = new double[n];
 arr = CreateArray(arr, n);
 OutputArray(arr, n);
 
-Console.WriteLine($"Разница = {RazMinMax(arr, n)}"); 
+Console.WriteLine($"Разница = {RazMinMax(arr, n)}");
 
-double[] CreateArray(double[] arr, int n) 
-{        
-    for (int i = 0; i < n; i++) 
+double[] CreateArray(double[] arr, int n)
+{
+    for (int i = 0; i < n; i++)
     {
         arr[i] = rnd.Next(-100, 100);
         arr[i] += Math.Round(rnd.NextDouble(), 1);
@@ -24,24 +24,23 @@ double[] CreateArray(double[] arr, int n)
     return arr;
 }
 
-void OutputArray(double[] arr, int n) 
-{         
+void OutputArray(double[] arr, int n)
+{
     Console.Write($"[{arr[0]}, ");
 
-    for (int i = 1; i < n - 1; i++) 
+    for (int i = 1; i < n - 1; i++)
     {
         Console.Write($"{arr[i]}, ");
     }
-    Console.WriteLine($"{arr[n-1]}]");
+    Console.WriteLine($"{arr[n - 1]}]");
 }
 
-
-double RazMinMax(double[] arr, int n) 
-{ 
+double RazMinMax(double[] arr, int n)
+{
     double min = arr[0];
     double max = arr[0];
 
-    for (int i = 1; i < n; i++) 
+    for (int i = 1; i < n; i++)
     {
         if (arr[i] < min) min = arr[i];
         if (arr[i] > max) max = arr[i];
@@ -50,17 +49,6 @@ double RazMinMax(double[] arr, int n)
     Console.WriteLine($"Максимальное число в массиве: {max}");
     Console.WriteLine($"Минимальное число в массиве: {min}");
 
-
-    double result = 0;
-    if (min < 0) 
-    {
-        min *= -1;
-        result = Math.Round(min * 2 - max, 1);
-    } 
-    else 
-    {
-        result = Math.Round(max - min, 1);
-    }
-
+    double result = max - min;
     return result;
 }
