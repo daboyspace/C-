2f79de0f7cba0aa95d2863e264c9bffa82933599
daboyspace <daboyspace@gmail.com>
2022-12-08 08:59:59 +0300

    Add Task 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -8,3 +8,41 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+Console.Write("Введите кол-во строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = CreateMatrixRndDouble(m, n, -10, 10);
+PrintMatrixDouble(array, "", "  ", "");
