950edf0fca88969a5ba8dceaee3d699135772a53
daboyspace <daboyspace@gmail.com>
2022-12-08 09:09:59 +0300

    Add Task 52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -6,3 +6,65 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+double[] AverageColumnsMatrix(int[,] matrix)
+{
+    double[] averageArray = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            averageArray[j] += matrix[i, j];
+        }
+        averageArray[j] = Math.Round(averageArray[j] / matrix.GetLength(0), 1);
+    }
+    return averageArray;
+}
+
+void PrintArrayDouble(double[] arr, string beginRow, string separatorElems, string endRow)
+{
+    Console.Write(beginRow);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}{separatorElems}");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine(endRow);
+}
+
+int[,] array = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array, "", "", "");
+
+double[] averageColumnsArray = AverageColumnsMatrix(array);
+PrintArrayDouble(averageColumnsArray, "Среднее арифметическое каждого столбца: ", "; ", ".");
