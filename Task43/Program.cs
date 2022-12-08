3707d98c5a47f40738f8c8e34b63eef12fe4cba9
daboyspace <daboyspace@gmail.com>
2022-12-08 09:33:40 +0300

    Add Task 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,36 +1,41 @@
-﻿// Задача 43. Напишите программу, которая найдёт 
-// точку пересечения двух прямых,заданных уравнениями 
-// y = k1 * x + b1, y = k2 * x + b2;
-// значения b1, k1, b2 и k2 задаются пользователем.
-
-// Например:
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-
-Console.WriteLine("Введите данные: ");
-
-Console.Write("b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-
-double y1 = Intersection(b1, k1);
-double y2 = Intersection(b2, k2);
-
-Console.WriteLine($"({y1}; {y2})");
-
-double Intersection(double b, double k)
+double[] StringToNumbersArray(string str, string separator)
 {
-    double x = -0.5;
+    string[] arrString = str.Split($"{separator}");
+    double[] arr = new double[arrString.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToDouble(arrString[i]);
+    }
+    return arr;
+}
 
-    double y = k * x + b;
+double EquationX(double b1, double k1, double b2, double k2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    return x;
+}
 
+double EquationY(double b1, double k1, double b2, double k2)
+{
+    double y = k2 * (-(b1 - b2) / (k1 - k2)) + b2;
     return y;
 }
+
+Console.Write($"Введите значения b1, k1, b2 и k2 через запятую: ");
+string numbersString = Console.ReadLine();
+
+double[] array = StringToNumbersArray(numbersString, ",");
+
+double b1 = array[0];
+double k1 = array[1];
+double b2 = array[2];
+double k2 = array[3];
+
+double equationX = Math.Round(EquationX(b1, k1, b2, k2), 1);
+double equationY = Math.Round(EquationY(b1, k1, b2, k2), 1);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({equationX}; {equationY})");
